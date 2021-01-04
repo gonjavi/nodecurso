5491b27d7ae6115489ce81782e1428de711cba34
manci <javiercaliescali@gmail.com>
2021-01-04 16:17:12 -0500

    no arrow function - dentro de object

--- 02-fundamentos/flecha.js
+++ 02-fundamentos/flecha.js
@@ -5,9 +5,20 @@ function sumar(a,b) {
 console.log(sumar(10, 20));
 
 const sumar1 = (a,b) => a + b;
-
 console.log(sumar1(15, 20));
 
 const saludar = () => 'hola mundo';
-
 console.log(saludar());
+
+const sayHello = nombre => `Hola ${nombre}`;
+console.log(sayHello('Zora'));
+
+let pool = {
+  nombre: 'Olga',
+  apellido: 'Andrade',
+  instrumento: 'voz',
+  getNombre() {
+    return `${this.nombre} ${this.apellido} - instrumento ${this.instrumento}`
+  }
+};
+console.log(pool.getNombre());
