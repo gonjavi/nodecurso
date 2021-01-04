1d644a25f4998728b94333c787db3132b9a37112
manci <javiercaliescali@gmail.com>
2021-01-04 15:09:21 -0500

    let-var

---
+++ 02-fundamentos/let-var.js
@@ -0,0 +1,12 @@
+let nombre = ' Javier';
+
+if (true) {
+  nombre = 'Pedro';
+}
+console.log(nombre);
+
+for (var i=0; i <= 5; i++) {
+  console.log(`i igual a:  ${i}`);
+}
+
+console.log(i); // igual a 6
