9d3ba5e2a26c53eabcc5fdf4dc17a48e26d52c64
manci <javiercaliescali@gmail.com>
2021-01-05 11:14:05 -0500

    utilizando yargs para consola

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,10 +1,27 @@
+ const argv = require('yargs')
+  .command('listar', 'Imprime en consola la tabla de multiplicar', {
+    base: {
+      demand: true,
+      alias: 'b'
+    },
+    limite: {
+      alias: 'l',
+      default: 10
+    }   
+  })
+  .help()
+  .argv;
+
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let argv = process.argv;
-let parametro = argv[2];
-let base = parametro.split('=')[1];
+let argv2 = process.argv;
 
-crearArchivo(base)
+console.log('Limite', argv.limite);
+//console.log(argv2);
+/* let parametro = argv[2];
+let base = parametro.split('=')[1]; */
+
+/* crearArchivo(base)
   .then(archivo => console.log(`Archivo creado: ${archivo}`))
   .catch(err => console.log(err)); 
-  
+   */
