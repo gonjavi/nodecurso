61d2273654000d8ed98d9812a6b65d3c08567a92
manci <javiercaliescali@gmail.com>
2021-01-04 21:38:18 -0500

    promesas

---
+++ 02-fundamentos/promesas.js
@@ -0,0 +1,53 @@
+let empleados = [{
+  id: 1,
+  nombre: 'zoraima'
+},{
+  id: 2,
+  nombre: 'Carolina'
+},{
+  id: 3,
+  nombre: 'Juan'
+}];
+
+let salarios = [{
+  id: 1,
+  salario: 1000,
+},{
+  id: 2,
+  salario: 2000 
+}];
+
+let getEmpleado = id => {
+  return new Promise((resolve, reject) => {
+    const empleadoDB = empleados.find(empleado => empleado.id === id);  
+    if (!empleadoDB) {
+      reject(`No existe un empleado con el ID ${id}`);
+    } else {
+      resolve(empleadoDB);
+    }
+  });
+}
+
+
+let getSalario = empleado => {
+  return new Promise((resolve, reject) => {
+    const salarioDB = salarios.find(salario => salario.id === empleado.id);
+    if (!salarioDB) {
+      reject(`No se encontró un salario para el empleado ${empleado.nombre}`);
+    } else {
+      resolve({
+        nombre: empleado.nombre,
+        salario: salarioDB.salario,
+        id: empleado.id
+      });
+    }
+  });  
+}
+
+getEmpleado(2).then(empleado => {  
+  getSalario(empleado).then(resp => {
+    console.log(`El salario de ${resp.nombre} es: ${resp.salario} `);
+  }, err => console.log(err));
+}, err => console.log(err));
+
+
