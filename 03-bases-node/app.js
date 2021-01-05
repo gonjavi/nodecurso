7d3e382cd8227b9391212d1c70bb9ab28558f5c6
manci <javiercaliescali@gmail.com>
2021-01-05 09:05:06 -0500

    recibir valor por consola con process.argv

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,9 +1,10 @@
-
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = 5;
+let argv = process.argv;
+let parametro = argv[2];
+let base = parametro.split('=')[1];
 
 crearArchivo(base)
   .then(archivo => console.log(`Archivo creado: ${archivo}`))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err)); 
   
